393737db6301f0e22df7c2a1defa25a081661cb9
Pete Miller <mrpetemiller@gmail.com>
2015-10-24 19:01:31 -0400

    Added some stuff

---
+++ RemoteGit/src/com/petemiller/eclipse/ExampleClass.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.petemiller.eclipse;
+
+/**
+ * @author LikreL
+ *
+ */
+public class ExampleClass {
+
+	/**
+	 * 
+	 */
+	public ExampleClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ RemoteGit/src/com/petemiller/eclipse/iExample.java
@@ -0,0 +1,5 @@
+package com.petemiller.eclipse;
+
+public interface iExample {
+
+}
